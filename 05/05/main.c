1eb5933d96fb022ffbcad82b3dd47187c5fcce89
chaeyeonkang06 <hellokcy@sookmyung.ac.kr>
2025-10-02 16:04:35 +0900

    Update main.c

--- 05/05/main.c
+++ 05/05/main.c
@@ -9,32 +9,23 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int a, b;
-    char ch;
-    int r;
+    int answer=78;
+    int a;
+    int trial=0;
     
-    printf("계산식을 입력하시오.: ");
-    scanf("%i%c%i", &a, &ch, &b);
+    do {
+        printf("숫자를 맞춰보세요.: ");
+        scanf("%i", &a);
+        
+        trial++;
+        
+        if(a>answer)
+            printf("조금 더 작은 숫자 써봐여.\n");
+         else if(a<answer)
+             printf("조금 더 큰 숫자를 써봐영.\n");
+    } while (a!=answer);
     
-    switch(ch)
-    {
-        case '+':
-            r = a+b;
-            break;
-            
-        case '-':
-            r = a-b;
-            break;
-            
-        case '*':
-            r = a*b;
-            break;
-            
-        case '/':
-            r = a/b;
-            break;
-    }
-            printf("=%i\n", r);
+    printf("축하드립니당! 맞추셨네여. 시도횟수: %i.\n", trial);
     
     return 0;
 }
