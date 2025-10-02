6f12e9378b560831c33c2db1799c037b16ff3db6
chaeyeonkang06 <hellokcy@sookmyung.ac.kr>
2025-10-02 15:33:19 +0900

    Update main.c

--- 05/05/main.c
+++ 05/05/main.c
@@ -9,14 +9,14 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int a;
+    int a, b;
+    int sum=0;
     printf("정수 하나를 입력하시오.: ");
     scanf("%i", &a);
     
-    if (a>0)
-        printf("절대값은 %i 입니다.\n", a);
-         else
-            printf("절대값은 %i 입니다.\n", a*(-1));
-        
+    for(b=1;b<=a;b++)
+        sum = sum + b;
+     
+        printf("합은 %i 입니다.\n", sum);
     return 0;
 }
